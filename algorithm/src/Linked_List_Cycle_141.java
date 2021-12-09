066525bcbeec912e822774050378ceff75c87e3e
Caroline Sun <carolinesun@Carolines-MacBook-Pro.local>
2021-12-08 22:12:46 -0500

    linked list ccycle

---
+++ algorithm/src/Linked_List_Cycle_141.java
@@ -0,0 +1,33 @@
+/*
+ Definition for singly-linked list.
+ 1. create map
+ 2. iterate linked list
+ if !map.contains(node) -> put
+ else return true
+ */
+import java.lang.*;
+import java.util.HashMap;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+         val = x;
+         next = null;
+    }
+ }
+
+public class Linked_List_Cycle_141 {
+    public boolean hasCycle(ListNode head) {
+        HashMap<ListNode, Integer> map = new HashMap<>();
+        while (head != null) {
+            if (!map.containsKey(head)) {
+                map.put(head, 1);
+                head = head.next;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
