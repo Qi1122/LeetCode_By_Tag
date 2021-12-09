97811cdb7eaeed8eb452b90aeac7455f8d04920b
Caroline Sun <carolinesun@Carolines-MacBook-Pro.local>
2021-12-08 22:40:39 -0500

    remove linked list elements

---
+++ algorithm/src/Remove_Elements_203.java
@@ -0,0 +1,16 @@
+public class Remove_Elements_203 {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        dummy.next = head;
+        while (head != null) {
+            if (head.val == val) {
+                prev.next = head.next;
+            } else {
+                prev = head;
+            }
+            head = head.next;
+        }
+        return dummy.next;
+    }
+}
