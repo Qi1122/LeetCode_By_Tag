f48d1b4c7f0f2545555dc5c09c0fe1706f7a70ac
Caroline Sun <carolinesun@Carolines-MacBook-Pro.local>
2021-12-08 22:47:00 -0500

    duplicate & reverse

---
+++ algorithm/src/Reverse_Linked_List_206.java
@@ -0,0 +1,13 @@
+public class Reverse_Linked_List_206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
