2d4be728b68dea4b80da78f798502f24c2fed340
Caroline Sun <carolinesun@Carolines-MacBook-Pro.local>
2021-12-21 10:36:43 -0500

    add comments

--- algorithm/src/Course_Schedule_I_207.java
+++ algorithm/src/Course_Schedule_I_207.java
@@ -1,16 +1,20 @@
 /*
 https://leetcode.com/problems/course-schedule/
 
-graph: construct a graph from giving inputs, graph构建的是点边的关系，key -> 点，value -> 相邻的所有点
+-- graph: construct a graph from giving inputs, graph构建的是点边的关系，key -> 点，value -> 相邻的所有点
+-- 维持一个记录vertices indegree的list, indegree表示每一门课作为end的入度
 
-1. construct graph -> add each course to graph
+1. 记录vertices indegree -> int[] indegree = new int[numCourses];
+   when indegree[i] = 0 -> add to queue -> 扩展 -> update indegree
+2. construct graph -> add each course to graph
                    -> for each course, add prerequisities to graph:
                                     graph.get(start).add(end);
                                     indegree[end]++;
-2. for each element in indegree, do BFS
+3. for each element in indegree, do BFS
+indegree[q.poll] = 0 -> update count
 queue -> put indegree[i] == 0 into queue, 可以从这个点扩展出去
-3. traverse indegree, put indegree[i] == 0 in queue
-4. find neighbours -> if nei in graph, indegree[nei]--
+4. traverse indegree, put indegree[i] == 0 in queue
+5. find neighbours -> if nei in graph, indegree[nei]--
                -> indegree[nei] == 0, add to queue
                -> if indegree[i] == 0 -> can take course -> update result
 */
@@ -19,15 +23,15 @@ import java.util.*;
 public class Course_Schedule_I_207 {
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
         Map<Integer, List<Integer>> graph = new HashMap<>();
         int count = 0;
-        int[] indegree = new int[numCourses];
         for (int i = 0; i < numCourses; i++) {
             graph.put(i, new ArrayList<Integer>());
         }
         for (int i = 0; i < prerequisites.length; i++) {
-            int start = prerequisites[i][0];
-            int end = prerequisites[i][1];
+            int start = prerequisites[i][1];
+            int end = prerequisites[i][0];
             graph.get(start).add(end);
             indegree[end]++;
         }
@@ -36,6 +40,12 @@ public class Course_Schedule_I_207 {
         for (int i = 0; i < indegree.length; i++) {
             if (indegree[i] == 0) q.offer(i);
         }
+        /*
+        DO NOT use foreach loop, will offer wrong element to queue
+        for (int item : indegree) {
+            if (item == 0) q.offer(item);
+        } 报错原因：这里需要对index进行操作，被加入q的是index，而不是元素本身
+         */
         while (!q.isEmpty()) {
             int cur = q.poll();
             count++;
