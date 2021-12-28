70d37a61293d30e03419923071ed728c90d5b604
Caroline Sun <carolinesun@Carolines-MacBook-Pro.local>
2021-12-28 16:25:44 -0500

    bt find distance

---
+++ algorithm/src/BT_Find_DIstance_1740.java
@@ -0,0 +1,32 @@
+/*
+https://leetcode.com/problems/find-distance-in-a-binary-tree/
+
+ */
+public class BT_Find_DIstance_1740 {
+    public int findDistance(TreeNode root, int p, int q) {
+        int res = 0;
+        TreeNode lca = LCA(root, p, q);
+        return findDistance(lca, p) + findDistance(lca, q) - 2;
+        //each path added one more point, need to - both
+    }
+
+    private TreeNode LCA(TreeNode root, int p, int q) {
+        if (root == null || root.val == p || root.val == q) return root;
+        TreeNode left = LCA(root.left, p, q);
+        TreeNode right = LCA(root.right, p, q);
+        if (left != null && right != null) return root;
+        if (left != null) return left;
+        if (right != null) return right;
+        return null;
+    }
+
+    private int findDistance(TreeNode root, int target) {
+        if (root == null) return 0;
+        if (root.val == target) return 1;
+        int left = findDistance(root.left, target);
+        int right = findDistance(root.right, target);
+        if (left != 0) return left + 1; //need to add the current point
+        if (right != 0) return right + 1;
+        return 0;
+    }
+}
---
+++ algorithm/src/BT_Longest_Zigzag_1372.java
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/
+ */
+public class BT_Longest_Zigzag_1372 {
+    private int maxZ = Integer.MIN_VALUE;
+
+    public int longestZigZag(TreeNode root) {
+        dfs(root);
+        return maxZ - 1;
+    }
+
+    private int[] dfs(TreeNode root) {
+        int[] res = new int[2];
+        if (root == null) return res;
+        //left: longest zigzag can get from left
+        //right: longest zigzag can get from right
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+        //need to add current node
+        res[0] = left[1] + 1;
+        res[1] = right[0] + 1;
+        maxZ = Math.max(maxZ, Math.max(res[0], res[1]));
+        return res;
+    }
+}
